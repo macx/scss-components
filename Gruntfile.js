0c4a46578ee43cf3c8a15940d95352fe41fa5995
Tim Hartmann <tim.hartmann93@gmail.com>
2013-09-13 18:53:50 +0200

    Initial commit
    -- added grunt and bower
    -- added some tests
    -- function for matching icons
    -- mixins for triangle, columns, icons, fonts, respond-to, clearfix

---
+++ Gruntfile.js
@@ -0,0 +1,49 @@
+module.exports = function(grunt) {
+  grunt.initConfig({
+    pkg: grunt.file.readJSON('package.json'),
+    watch: {
+      options: {
+        livereload: true
+      },
+      files: [
+        'tests/assets/**'
+      ],
+      tasks: ['default']
+    },
+    sass: {
+      test: {
+        options: {
+          style: 'expanded',
+          compass: true
+        },
+        files: [
+          {
+            expanded: true,
+            src: ['tests/assets/scss/application.scss'],
+            dest: 'tests/assets/stylesheets/application.css',
+            filter: 'isFile'
+          }
+        ]
+      }
+    },
+    bumpup: ['package.json', 'bower.json'],
+    tagrelease: {
+      file: 'package.json',
+      commit:  true,
+      message: 'Release %version%',
+      prefix:  '',
+      annotate: false
+    }
+  });
+
+  grunt.loadNpmTasks('grunt-contrib');
+  grunt.loadNpmTasks('grunt-tagrelease');
+  grunt.loadNpmTasks('grunt-bumpup');
+
+  grunt.registerTask('default', ['sass']);
+  grunt.registerTask('release', function (type) {
+    type = type ? type : 'patch';
+    grunt.task.run('bumpup:' + type);
+    grunt.task.run('tagrelease');
+  });
+};
